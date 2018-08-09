7563959dd1c979507df92c3a92e7310db30012de
FrauDeborah <FrauDeborah@gmail.com>
2018-08-09 15:50:59 -0600

    inp progress can log page titles

--- src/app/my-nav.service.ts
+++ src/app/my-nav.service.ts
@@ -9,33 +9,16 @@ export class MyNavService {
     private _activatedRoute: ActivatedRoute
   ) { }
 
-  pageTitle:string;
+  pageTitle:string = "Welcome to Quizki";
+
+  setPageTitle(newPageTitle) {
+    this.pageTitle = newPageTitle;
+    console.log("my-nav.service.ts in setPageTitle: " + this.pageTitle);
+  };
 
   getPageTitle() {
-    /* Remove the following line after the resolution. Notice you should see
-     * Route(url:'', path:'') on the console. That's because my-nav
-     * activated the service. Each page component has a data element
-     *  data:{pageTitle, "thePageTitle"}.
-     */
-
-    console.log(this._activatedRoute.toString());
-
-    /* The following statment is correct once we have data to display.
-     * Currently, the pageTitle displays as ''. I think we need to
-     * use lifecycle hooks and resolve. The big question is to determine where
-     * to place the lifecyle hook and resolve, and which lifecyle hook to use.
-     * Note that the ngOnInit occurs before the activatedRoute has a url assigned.
-     * likely we need to use ngOnChange, or at least that's my current thought.
-     * Perhaps each new page component needs to call getPageTitle() so the focus
-     * is on that particular page. Or perhaps associate with the button click.
-     * I understand mulpile actions on buttons are possible, but
-     * the practice is considered obstructive programming.
-     */
-
-    //this.pageTitle = this._activatedRoute.snapshot.data['pageTitle'];
-
-    this.pageTitle = "myTempGetPageTitle";
-
+  //  this.pageTitle = this._activatedRoute.snapshot.data['pageTitle'];
+    console.log("my-nav.service.ts in getPageTitle: " + this.pageTitle);
     return this.pageTitle;
   };
 
--- src/app/my-nav/my-nav.component.ts
+++ src/app/my-nav/my-nav.component.ts
@@ -2,9 +2,10 @@
 
 import { Component } from '@angular/core';
 import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
+import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
 
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { filter, map, mergeMap } from 'rxjs/operators';
 
 import { MyNavService } from './../my-nav.service';
 
@@ -21,10 +22,34 @@ export class MyNavComponent {
       map(result => result.matches)
     );
 
+  navigationEnd: Observable<NavigationEnd>;
+//  activatedRoute: ActivatedRoute;
+
   constructor(
     private breakpointObserver: BreakpointObserver,
-    private myNavService: MyNavService
-    ) {}
+    private myNavService: MyNavService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+    private pageTitleService: MyNavService
+    ) {
+      // Create an Observable NavigationEnd event
+      this.navigationEnd = this.router.events.pipe(
+        filter((navigationEndEvent) => navigationEndEvent instanceof NavigationEnd),
+        map(() => this.activatedRoute),
+        map((route) => {
+          while (route.firstChild) route = route.firstChild;
+          return route;
+        }),
+        filter((route) => route.outlet === 'primary'),
+        mergeMap((route) => route.data)
+      ) as Observable<NavigationEnd>;
+    }
+
+    ngOnInit() {
+      this.navigationEnd.subscribe(evt => console.log('Navigation Ended!'));
+      this.navigationEnd.subscribe(navigationEndEvent => this.pageTitleService.setPageTitle(navigationEndEvent['pageTitle']))
+      this.navigationEnd.subscribe(evt => console.log(this.pageTitleService));
+    }
 
     pageTitle = this.myNavService.getPageTitle();
 
